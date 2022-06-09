36c358b2ca75e6885eadbfcd27f8a3c2b68240c4
DMonget <d.monget@betclicgroup.com>
2022-06-09 14:50:11 +0000

    add listBucketContent function

--- helloworld-go/hello-world/go.mod
+++ helloworld-go/hello-world/go.mod
@@ -1,8 +1,10 @@
-require github.com/aws/aws-lambda-go v1.23.0
+require (
+	github.com/aws/aws-lambda-go v1.23.0
+	github.com/aws/aws-sdk-go v1.44.30
+)
 
 replace gopkg.in/yaml.v2 => gopkg.in/yaml.v2 v2.2.8
 
 module hello-world
 
 go 1.16
-
--- helloworld-go/hello-world/main.go
+++ helloworld-go/hello-world/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 var (
@@ -21,7 +26,42 @@ var (
 	ErrNon200Response = errors.New("Non 200 Response found")
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+type App struct {
+	S3 *s3.S3
+}
+
+func (app App) listBucketContent(bucketName string) string {
+	req, err := app.S3.ListObjects(&s3.ListObjectsInput{
+		Bucket: aws.String(bucketName),
+	})
+
+	if err != nil {
+		log.Fatalf("Unable list objects in %q, %v", bucketName, err)
+	}
+
+	out, err := json.Marshal(req)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(out)
+}
+
+func (app App) retrieveFileSize(bucketName, fileName string) string {
+
+	rawData, err := app.S3.GetObject(
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(fileName),
+		})
+
+	if err != nil {
+		log.Fatalf("Unable to get object %q, %v", fileName, err)
+	}
+	return fmt.Sprint("Downloaded", rawData.Metadata, rawData.ContentLength, "bytes")
+}
+
+func (app App) handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	resp, err := http.Get(DefaultHTTPGetAddress)
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
@@ -41,11 +81,21 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		Body:       app.listBucketContent("testperf-bucket"),
 		StatusCode: 200,
 	}, nil
 }
 
 func main() {
-	lambda.Start(handler)
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to create AWS session, %v", err)
+	}
+
+	s3 := s3.New(sess)
+	app := App{S3: s3}
+	lambda.Start(app.handler)
 }
