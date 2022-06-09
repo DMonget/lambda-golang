d36113927a56a6a00be79d7959cdf9849e9eed9d
Franck Chaux <franck.chaux@gmail.com>
2022-06-09 15:48:44 +0200

    [UPDATE] Count each file into S3

--- helloworld-go/node-test/app.js
+++ helloworld-go/node-test/app.js
@@ -3,6 +3,10 @@ var AWS = require('aws-sdk');
 var csv = require('csvtojson');
 var S3 = new AWS.S3({apiVersion: '2006-03-01'});
 var params = {Bucket: 'testperf-bucket', Key: 'test.csv'};
+var params1000 = {Bucket: 'testperf-bucket', Key: 'test-1000.csv'};
+var params2000 = {Bucket: 'testperf-bucket', Key: 'test-2000.csv'};
+var params5000 = {Bucket: 'testperf-bucket', Key: 'test-5000.csv'};
+var params10000 = {Bucket: 'testperf-bucket', Key: 'test-10000.csv'};
 
 let response;
 
@@ -32,13 +36,57 @@ exports.lambdaHandler = async (event, context) => {
             return json;
         }
 
+        let data1000 = async function() {
+            // get csv file and create stream
+            const stream = S3.getObject(params1000).createReadStream();
+            // convert csv file (stream) to JSON format data
+            const json = await csv().fromStream(stream);
+            
+            return json;
+        }
+
+        let data2000 = async function() {
+            // get csv file and create stream
+            const stream = S3.getObject(params2000).createReadStream();
+            // convert csv file (stream) to JSON format data
+            const json = await csv().fromStream(stream);
+            
+            return json;
+        }
+
+        let data5000 = async function() {
+            // get csv file and create stream
+            const stream = S3.getObject(params5000).createReadStream();
+            // convert csv file (stream) to JSON format data
+            const json = await csv().fromStream(stream);
+            
+            return json;
+        }
+
+        let data10000 = async function() {
+            // get csv file and create stream
+            const stream = S3.getObject(params10000).createReadStream();
+            // convert csv file (stream) to JSON format data
+            const json = await csv().fromStream(stream);
+            
+            return json;
+        }
+
         let csvData = await data();
+        let csvData1000 = await data1000();
+        let csvData2000 = await data2000();
+        let csvData5000 = await data5000();
+        let csvData10000 = await data10000();
 
         // const ret = await axios(url);
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                csvData
+                'count200' : csvData.length,
+                'count1000' : csvData1000.length,
+                'count2000' : csvData2000.length,
+                'count5000' : csvData5000.length,
+                'count10000' : csvData10000.length,
             })
         }
     } catch (err) {
