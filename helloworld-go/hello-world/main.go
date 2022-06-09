3675b3bc5bf13a59ad1fccc8f0e31c8d906afa2d
DMonget <d.monget@betclicgroup.com>
2022-06-09 15:39:58 +0000

    Create ParseAndCount func

--- helloworld-go/hello-world/main.go
+++ helloworld-go/hello-world/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -28,6 +29,60 @@ var (
 
 type App struct {
 	S3 *s3.S3
+}
+
+type CsvEntry struct {
+	ColA string
+	ColB string
+	ColC string
+	ColD string
+	ColE string
+	ColF string
+	ColG string
+	ColH string
+	ColI string
+	ColJ string
+}
+
+type FileParseResult struct {
+	FileName string
+	RowCount int
+}
+
+func parseCsvEntry(data [][]string) []CsvEntry {
+	var entryList []CsvEntry
+	for i, line := range data {
+		if i > 0 { // omit header line
+			var entry CsvEntry
+			for j, field := range line {
+				if j == 0 {
+					entry.ColA = field
+				} else if j == 1 {
+					entry.ColB = field
+				} else if j == 2 {
+					entry.ColB = field
+				} else if j == 3 {
+					entry.ColB = field
+				} else if j == 4 {
+					entry.ColB = field
+				} else if j == 5 {
+					entry.ColB = field
+				} else if j == 6 {
+					entry.ColB = field
+				} else if j == 7 {
+					entry.ColB = field
+				} else if j == 8 {
+					entry.ColB = field
+				} else if j == 9 {
+					entry.ColB = field
+				} else if j == 10 {
+					entry.ColB = field
+				}
+			}
+			entryList = append(entryList, entry)
+		}
+	}
+	return entryList
 }
 
 func (app App) listBucketContent(bucketName string) string {
@@ -61,6 +116,40 @@ func (app App) retrieveFileSize(bucketName, fileName string) string {
 	return fmt.Sprint("Downloaded", rawData.Metadata, rawData.ContentLength, "bytes")
 }
 
+func (app App) parseAndCount(bucketName string) []FileParseResult {
+	req, err := app.S3.ListObjects(&s3.ListObjectsInput{
+		Bucket: aws.String(bucketName),
+	})
+
+	if err != nil {
+		log.Fatalf("Unable list objects in %q, %v", bucketName, err)
+	}
+
+	var fileParseResults []FileParseResult
+	for _, file := range req.Contents {
+		var entryList []CsvEntry
+
+		rawData, err := app.S3.GetObject(
+			&s3.GetObjectInput{
+				Bucket: aws.String(bucketName),
+				Key:    aws.String(*file.Key),
+			})
+
+		if err != nil {
+			log.Fatalf("Unable to get object %q, %v", *file.Key, err)
+		}
+
+		csvReader := csv.NewReader(rawData.Body)
+		data, err := csvReader.ReadAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+		entryList = parseCsvEntry(data)
+		fileParseResults = append(fileParseResults, FileParseResult{FileName: *file.Key, RowCount: len(entryList)})
+	}
+	return fileParseResults
+}
+
 func (app App) handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	resp, err := http.Get(DefaultHTTPGetAddress)
 	if err != nil {
@@ -80,8 +169,14 @@ func (app App) handler(request events.APIGatewayProxyRequest) (events.APIGateway
 		return events.APIGatewayProxyResponse{}, ErrNoIP
 	}
 
+	parseResults := app.parseAndCount("testperf-bucket")
+	out, err := json.Marshal(parseResults)
+	if err != nil {
+		panic(err)
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body:       app.listBucketContent("testperf-bucket"),
+		Body:       string(out),
 		StatusCode: 200,
 	}, nil
 }
